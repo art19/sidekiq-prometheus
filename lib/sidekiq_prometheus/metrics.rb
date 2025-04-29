1ba9410f383ea09f52a5b3b2c3da1f48c2eafe40
Mike Li <lmike@amazon.com>
2025-04-29 07:31:17 -0700

    return nil for unregister

--- lib/sidekiq_prometheus/metrics.rb
+++ lib/sidekiq_prometheus/metrics.rb
@@ -191,7 +191,7 @@ module SidekiqPrometheus::Metrics
   def unregister(name:)
     # metric mmap doesn't support unregister
     # registry.unregister(name.to_sym)
-    false
+    nil
   end
 
   class InvalidMetricType < StandardError; end
